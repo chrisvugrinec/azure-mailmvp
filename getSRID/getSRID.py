5ff999bbdcd12303e9a4de7cd5f7ac4661359ee8
gogsadmin <RedHat$1>
2018-01-14 17:48:37 +0100

    bfix

--- getSRID/getSRID.py
+++ getSRID/getSRID.py
@@ -1,13 +1,24 @@
-subject = "hello world : SRID: 12343324 I am not a bankster"
+import os
+import json
 
-try:  
-    subjectSRIDIndex= subject.index('SRID')+6
-    subSubject = subject[subjectSRIDIndex:]
-    subSubjectIndex= len(subSubject) - subSubject.index(' ')+1
-    result = subSubject[0: subSubject.index(' ')+1: ]
+with open(os.environ["REQ"]) as req:
+    subject = json.loads(req.read())
+
+response = open(os.environ['res'], 'w')  
+
+try:
+    subjectSRIDIndex= subject["subject"].index('SR')+3
+    subSubject = subject["subject"][subjectSRIDIndex:]
+    try:
+        subSubjectIndex= subSubject.index(' ')+1
+        result = subSubject[0: subSubject.index(' ')+1: ]
+    except:
+        result = subSubject
+    print(result)
 except:
     result = "-1"
+finally:
+    response.write(result)  
 
-#print(subject)
-#print(subSubject)
-print(result)
+response.close() 
+
